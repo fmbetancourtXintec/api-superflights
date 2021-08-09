a4fe2365ddc6d550dbc7d409696119001b65eb2d
Freddy Moreno Betancourt <freddy.moreno@xintec.cl>
2021-08-09 13:59:38 -0400

    33. Crear Usuario

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 
-export class UserDto{
+export class UserDTO{
   @IsNotEmpty()
   @IsString()
   readonly name: string;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { UserDTO } from "./dto/user.dto";
+import { IUser } from "../common/interfaces/user.inteface";
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from "@nestjs/mongoose";
+import { USER } from "../common/models/models";
+import { Model } from "mongoose";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  constructor(@InjectModel(USER.name) private readonly model: Model<IUser>) {
+  }
+
+  async hashPassword(password: string): Promise<string>{
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  async create(userDTO: UserDTO): Promise<IUser>{
+    // try {
+      const hash = await this.hashPassword(userDTO.password);
+      const newUser = new this.model({...userDTO, password: hash});
+      return await newUser.save();
+    // }catch (e) {
+    //   console.log("aqui!");
+    // }
+  }
+}
