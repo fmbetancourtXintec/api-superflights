0aacc063419627ad319487807c46b18278fd3df1
Freddy Moreno Betancourt <freddy.moreno@xintec.cl>
2021-08-18 22:04:42 -0400

    47. Crear Vuelo

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { FlightService } from "./flight.service";
+import { FlightDTO } from "./dto/flight.dto";
 
-@Controller('flight')
-export class FlightController {}
+@Controller('api/v1/flight')
+export class FlightController {
+
+  constructor(private readonly flightService: FlightService) {
+  }
+
+  @Post()
+  create(@Body() flightDTO: FlightDTO){
+    return this.flightService.create(flightDTO);
+  }
+}
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from "@nestjs/mongoose";
+import { FLIGHT } from "../common/models/models";
+import { Model } from "mongoose";
+import { IFlight } from "../common/interfaces/flight.inteface";
+import { FlightDTO } from "./dto/flight.dto";
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+
+  constructor(@InjectModel(FLIGHT.name) private readonly model: Model<IFlight>) {
+  }
+
+  async create (flightDTO: FlightDTO): Promise<IFlight>{
+    const newFlight = new this.model(flightDTO);
+    return await newFlight.save();
+  }
+
+}
